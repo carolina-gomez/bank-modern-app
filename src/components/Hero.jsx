451e026c87f83db1be6419ed4a10c377e1d65a7a
Carolina Gomez <carolinagomezTX@gmail.com>
2022-08-16 22:17:37 -0500

    edited Payment Method

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ import GetStarted from './GetStarted'
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
       <div className={`flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2`}>
         <img src={discount} alt="discount" className='w-[32px] h-[32px]'/>
         <p className={`${styles.paragraph} ml-2`}>
@@ -26,6 +27,10 @@ const Hero = () => (
           <GetStarted />
         </div>
       </div>
+
+      <h1 className='flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+        Payment Method
+      </h1>
 
     </div>
   </section>
